ccdec479b5ff9f790c202b81d56cfab26832c474
Andres Jimenez <andresje17@gmail.com>
2021-07-01 23:57:40 -0600

    :hammer: Test pokemon stats from API

--- poke_api.py
+++ poke_api.py
@@ -10,7 +10,7 @@ def get_pokemon():
   http = urllib3.PoolManager()
   res = http.request('GET', 'https://pokeapi.co/api/v2/pokemon/bulbasaur')
   json_data = json.loads(res.data.decode('utf-8'))
-  print(json_data)
+  return json_data
 
 def hp_stat(base_stat, iv, ev, lvl):
   return math.floor(((2 * base_stat + iv + math.floor(ev / 4)) * lvl) / 100) + lvl + 10
@@ -18,8 +18,18 @@ def hp_stat(base_stat, iv, ev, lvl):
 def other_stat(base_stat, iv, ev, lvl, nature):
   return math.floor((hp_stat(base_stat, iv, ev, lvl) - lvl - 5) * nature)
 
+def calculate_stats(pokemon):
+  pokemon_stats = pokemon['stats']
+  total_stats = []
+  iv, ev = 0
+  lvl = 50
+  nature = 1
+  return total_stats
+
 def main():
   print('MAIN')
+  pokemon = get_pokemon()
+  print(pokemon['stats'])
 
 
 if __name__ == '__main__':
