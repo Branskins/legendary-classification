14698ce1049d412a7dfdd582ef38305006426487
Andres Jimenez <andresje17@gmail.com>
2021-07-03 22:10:25 -0600

    :hammer: Add classes for every nature

--- poke_api.py
+++ poke_api.py
@@ -2,6 +2,19 @@ import urllib3
 import json
 import math
 import random
+from personality_value_nature import p_value_nature
+
+def create_nature_instance(nature_name):
+  module = __import__('nature')
+  class_ = getattr(module, nature_name)
+  instance = class_()
+  return instance
+
+def define_nature():
+  p_value = personality_value()
+  nature_name = p_value_nature[p_value]
+  nature_instance = create_nature_instance(nature_name)
+  return nature_instance
 
 def personality_value():
   return random.randint(0, 254) % 25
@@ -23,20 +36,25 @@ def calculate_stats(pokemon):
   total_stats = {}
   iv = ev = 0
   lvl = 50
-  nature = 1
+  nature_instance = define_nature()
+  print(f'Pokemon nature: {type(nature_instance).__name__}')
+
   for stat in pokemon_stats:
     stat_value = stat['base_stat']
     stat_name = stat['stat']['name']
-    total_stats[stat_name] = other_stat(stat_value, iv, ev, lvl, nature)
+    nature = nature_instance.multiplier(stat_name)
+    if stat_name == 'hp':
+      total_stats[stat_name] = hp_stat(stat_value, iv, ev, lvl)
+    else:
+      total_stats[stat_name] = other_stat(stat_value, iv, ev, lvl, nature)
   
   return total_stats
 
 def main():
   print('MAIN')
-  pokemon = get_pokemon()
-  total_stats = calculate_stats(pokemon)
-  print(total_stats)
-
+  bulbasaur = get_pokemon()
+  stats = calculate_stats(bulbasaur)
+  print(stats)
 
 if __name__ == '__main__':
   main()
