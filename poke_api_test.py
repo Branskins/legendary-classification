34daa8ecc38a52d111210fd1bd806e4dfbb85f4f
Andres Jimenez <andresje17@gmail.com>
2021-07-12 23:26:58 -0600

    :hammer: Add test to save data into CSV

--- poke_api_test.py
+++ poke_api_test.py
@@ -1,19 +1,18 @@
-def main():
+from db_populate import save_pokemon_to_csv
+
+def pokemon_resources():
   pokemon_main_piece = []
 
-  pokemon_piece_1 = []
-  pokemon_piece_1.append({'name': 'wobbuffet', 'url': 'https://pokeapi.co/api/v2/pokemon/202/'})
-  pokemon_piece_1.append({'name': 'girafarig', 'url': 'https://pokeapi.co/api/v2/pokemon/203/'})
+  pokemon_main_piece.append({'name': 'wobbuffet', 'url': 'https://pokeapi.co/api/v2/pokemon/202/'})
+  pokemon_main_piece.append({'name': 'girafarig', 'url': 'https://pokeapi.co/api/v2/pokemon/203/'})
+  pokemon_main_piece.append({'name': 'pineco', 'url': 'https://pokeapi.co/api/v2/pokemon/204/'})
+  pokemon_main_piece.append({'name': 'forretress', 'url': 'https://pokeapi.co/api/v2/pokemon/205/'})
 
-  pokemon_piece_2 = []
-  pokemon_piece_2.append({'name': 'pineco', 'url': 'https://pokeapi.co/api/v2/pokemon/204/'})
-  pokemon_piece_2.append({'name': 'forretress', 'url': 'https://pokeapi.co/api/v2/pokemon/205/'})
+  return pokemon_main_piece
 
-  pokemon_main_piece.append(pokemon_piece_1)
-  pokemon_main_piece.append(pokemon_piece_2)
-
-  print(pokemon_main_piece)
-  print(len(pokemon_main_piece))
+def main():
+  pokemon = pokemon_resources()
+  save_pokemon_to_csv(pokemon)
 
 if __name__ == '__main__':
   main()
