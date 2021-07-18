0d5daf762624e64f15080afd1d58a0e6dbf3de71
Andres Jimenez <andresje17@gmail.com>
2021-07-17 23:47:11 -0600

    :hammer: Start distance matrix calculation

--- clustering.py
+++ clustering.py
@@ -1,11 +1,25 @@
+import numpy as np
+
 from db_populate import read_stats_from_csv
 
-def disimilitud(pokemon_stats):
-  pass
+
+def disimilitud():
+    pokemon_stats = read_stats_from_csv()
+    names = pokemon_stats.iloc[:, -1]
+    stats = pokemon_stats.iloc[:, :-1].to_numpy()
+    dis = np.ones(stats.shape)
+    rows = stats.shape[0]
+
+    for i in range(rows):
+        # np.sqrt(((x[0] - x[0:5, ]) ** 2).sum(axis=1))
+        np.sqrt(((stats[i] - stats[i:rows, ]) ** 2).sum(axis=1))
+
+    return dis
+
 
 def main():
-  pass
+    return 1
 
 
 if __name__ == '__main__':
-  main()
+    values = main()
