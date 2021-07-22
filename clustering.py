9bed021b3a930f2500ba252ba6a4cbd868874910
Andres Jimenez <andresje17@gmail.com>
2021-07-21 22:48:44 -0600

    :hammer: Update distance matrix function

--- clustering.py
+++ clustering.py
@@ -3,18 +3,17 @@ import numpy as np
 from db_populate import read_stats_from_csv
 
 
-def disimilitud():
+def distance():
     pokemon_stats = read_stats_from_csv()
     names = pokemon_stats.iloc[:, -1]
     stats = pokemon_stats.iloc[:, :-1].to_numpy()
-    dis = np.ones(stats.shape)
     rows = stats.shape[0]
+    distance_matrix = np.ones((rows, rows))
 
     for i in range(rows):
-        # np.sqrt(((x[0] - x[0:5, ]) ** 2).sum(axis=1))
-        np.sqrt(((stats[i] - stats[i:rows, ]) ** 2).sum(axis=1))
+        distance_matrix[i] = np.sqrt(((stats[i] - stats[0:rows, ]) ** 2).sum(axis=1))
 
-    return dis
+    return distance_matrix
 
 
 def main():
