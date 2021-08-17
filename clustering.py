d0db2d30593e05ce2a3677b4035816b8b7e68c01
Andres Jimenez <andresje17@gmail.com>
2021-08-16 22:41:11 -0600

    :hammer: Add hierarchical clustering

--- clustering.py
+++ clustering.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.cluster import AgglomerativeClustering
 
 from db_populate import read_stats_from_csv
 
@@ -17,7 +18,11 @@ def distance():
 
 
 def main():
-    return 1
+    pokemon_stats = read_stats_from_csv()
+    names = pokemon_stats.iloc[:, -1]
+    stats = pokemon_stats.iloc[:, :-1].to_numpy()
+
+    clustering = AgglomerativeClustering(linkage='single').fit(stats)
 
 
 if __name__ == '__main__':
