a29e48ae0259c243e0375da51bf1cf63be1095e7
Andres Jimenez <andresje17@gmail.com>
2021-08-17 23:13:44 -0600

    :hammer: Save modeling results
    - Update to ward linkage

--- clustering.py
+++ clustering.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from sklearn.cluster import AgglomerativeClustering
 
 from db_populate import read_stats_from_csv
@@ -18,12 +19,18 @@ def distance():
 
 
 def main():
+    # Load and select the stats
     pokemon_stats = read_stats_from_csv()
-    names = pokemon_stats.iloc[:, -1]
-    stats = pokemon_stats.iloc[:, :-1].to_numpy()
+    stats = pokemon_stats.iloc[:, :-1]
 
-    clustering = AgglomerativeClustering(linkage='single').fit(stats)
+    # Create clustering model
+    clustering = AgglomerativeClustering().fit(stats)
+
+    # Concat the results with the stats
+    clustering_mapping_df = pd.concat([pokemon_stats, pd.Series(clustering.labels_, name='legendary')], axis=1)
+    # Save the results
+    clustering_mapping_df.to_csv('pokemon_results/results_ward_2021-08-17.csv')
 
 
 if __name__ == '__main__':
-    values = main()
+    main()
