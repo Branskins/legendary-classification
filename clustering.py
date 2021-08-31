2e088b05a43378d599d2944503cfc62a8baee5f2
Andres Jimenez <andresje17@gmail.com>
2021-08-30 22:40:39 -0600

    :hammer: Add more filtration criteria

--- clustering.py
+++ clustering.py
@@ -12,9 +12,15 @@ def cleanse_pokemon(pokemon):
     # Filtering Mega Evolutions
     criteria = pokemon['name'].map(lambda name: name.endswith('-mega'))
     dtf_pokemon = pokemon[criteria]
+    # Filtering Alolan forms
+    criteria = dtf_pokemon['name'].map(lambda name: name.endswith('-alola'))
+    dtf_pokemon = dtf_pokemon[criteria]
+    # Filtering Galarian forms
+    criteria = dtf_pokemon['name'].map(lambda name: name.endswith('-galar'))
+    dtf_pokemon = dtf_pokemon[criteria]
     # Filtering Gigantamax forms
-    criteria = pokemon['name'].map(lambda name: name.endswith('-gmax'))
-    dtf_pokemon = pokemon[criteria]
+    criteria = dtf_pokemon['name'].map(lambda name: name.endswith('-gmax'))
+    dtf_pokemon = dtf_pokemon[criteria]
 
     return dtf_pokemon
 
