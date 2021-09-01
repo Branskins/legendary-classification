b9a193c9d360ac14245142f38c89c932ea9ea625
Andres Jimenez <andresje17@gmail.com>
2021-08-31 23:04:22 -0600

    :building_construction: Create Data Loading folder

--- Data Loading/db_populate.py
+++ Data Loading/db_populate.py
@@ -1,12 +1,10 @@
 import csv
 import os
-from datetime import date
 import pandas as pd
+from datetime import date
 
-from poke_api import get_pokemon_resources
 
-
-def save_pokemon_resouces_to_csv(pokemon):
+def save_pokemon_resources_to_csv(pokemon):
     # Set CSV file name with today's date
     today = date.today().strftime('%Y-%m-%d')
     csv_name = f'pokemon_resources/pokemon_resources_{today}.csv'
@@ -32,13 +30,13 @@ def save_pokemon_stats_to_csv(pokemon):
         writer.writerows(pokemon)
 
 
-def read_resouces_from_csv():
+def read_resources_from_csv():
     files = []
     pokemon = []
     # Read all the CSV files
-    with os.scandir('pokemon_resources') as it:
+    with os.scandir('../pokemon_resources') as it:
         files = [(entry.name, entry.stat().st_mtime) for entry in it if entry.is_file() and entry.name.endswith('.csv')]
-    # Order by date, get the lastest date on top
+    # Order by date, get the latest date on top
     files = sorted(files, key=lambda entry: entry[1], reverse=True)
     csv_name = f'pokemon_resources/{files[0][0]}'
     # Read the most recent CSV file, make a list of dict
@@ -55,22 +53,12 @@ def read_stats_from_csv():
     files = []
     pokemon_df = pd.DataFrame()
     # Read all the CSV files
-    with os.scandir('pokemon_stats') as it:
+    with os.scandir('../pokemon_stats') as it:
         files = [(entry.name, entry.stat().st_mtime) for entry in it if entry.is_file() and entry.name.endswith('.csv')]
-    # Order by date, get the lastest date on top
+    # Order by date, get the latest date on top
     files = sorted(files, key=lambda entry: entry[1], reverse=True)
     csv_name = f'pokemon_stats/{files[0][0]}'
     # Read the most recent CSV file
     pokemon_df = pd.DataFrame(pd.read_csv(csv_name))
 
     return pokemon_df
-
-
-def main():
-    print('MAIN')
-    pokemon = read_stats_from_csv()
-    print(pokemon.head())
-
-
-if __name__ == '__main__':
-    main()
