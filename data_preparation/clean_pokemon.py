e2c86e37269b9a71fc816a1da6ec4cc106b1dde5
Andres Jimenez <andresje17@gmail.com>
2021-10-05 20:07:02 -0600

    :art: Change repetitive amount of filters to only one filter
    endswith() accepts a tuple of suffixes

--- data_preparation/clean_pokemon.py
+++ data_preparation/clean_pokemon.py
@@ -1,20 +1,8 @@
-import pandas as pd
+def delete_pokemon_forms(pokemon):
+    # Suffixes of pokemon forms
+    forms = ('-mega', '-alola', '-galar', '-gmax')
+    # Filtering Mega Evolutions, Alolan, Galarian and Gigantamax forms
+    criteria = pokemon['name'].map(lambda name: not name.endswith(forms))
+    df = pokemon[criteria]
 
-
-def cleanse_pokemon(pokemon):
-    dtf_pokemon = pd.DataFrame()
-
-    # Filtering Mega Evolutions
-    criteria = pokemon['name'].map(lambda name: name.endswith('-mega'))
-    dtf_pokemon = pokemon[criteria]
-    # Filtering Alolan forms
-    criteria = dtf_pokemon['name'].map(lambda name: name.endswith('-alola'))
-    dtf_pokemon = dtf_pokemon[criteria]
-    # Filtering Galarian forms
-    criteria = dtf_pokemon['name'].map(lambda name: name.endswith('-galar'))
-    dtf_pokemon = dtf_pokemon[criteria]
-    # Filtering Gigantamax forms
-    criteria = dtf_pokemon['name'].map(lambda name: name.endswith('-gmax'))
-    dtf_pokemon = dtf_pokemon[criteria]
-
-    return dtf_pokemon
+    return df
