f3dcb35f033d2cf0a039d68c4ffcb937c74e0704
Andres Jimenez <andresje17@gmail.com>
2021-10-05 20:09:42 -0600

    :hammer: Update concat() to bind filtered data and discard main

--- data_modeling/clustering.py
+++ data_modeling/clustering.py
@@ -3,8 +3,6 @@ import pandas as pd
 from sklearn.cluster import AgglomerativeClustering
 from datetime import date
 
-from data_loading.db_populate import read_stats_from_csv
-
 
 def distance(pokemon_stats):
     names = pokemon_stats.iloc[:, -1]
@@ -21,24 +19,18 @@ def distance(pokemon_stats):
 def load_clustering(pokemon_stats, linkage):
     # Select the stats only
     stats = pokemon_stats.iloc[:, :-1]
+    # When selecting rows on a DataFrame the index doesn't match with the labels
+    # It's necessary to reset the index to make a column bind
+    pokemon_stats = pokemon_stats.reset_index()
 
     # Create clustering model
     clustering = AgglomerativeClustering(linkage=linkage).fit(stats)
+    # Format clustering labels for concatenating
+    labels = pd.Series(clustering.labels_, name='legendary')
 
     # Concat the results with the stats
-    clustering_mapping_df = pd.concat([pokemon_stats, pd.Series(clustering.labels_, name='legendary')], axis=1)
+    clustering_mapping_df = pd.concat([pokemon_stats, labels], axis=1)
     # Save the results
     today = date.today().strftime('%Y-%m-%d')
     csv_name = f'pokemon_results/results_{linkage}_{today}.csv'
     clustering_mapping_df.to_csv(csv_name)
-
-
-def main():
-    pokemon_stats = read_stats_from_csv()
-    linkage_methods = ['ward', 'complete', 'average', 'single']
-    for linkage in linkage_methods:
-        load_clustering(pokemon_stats, linkage)
-
-
-if __name__ == '__main__':
-    main()
