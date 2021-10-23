e78175521aaf7160029d2033bd8349f398d40e4e
Andres Jimenez <andresje17@gmail.com>
2021-10-22 21:24:01 -0600

    :hammer: Add filtering for Pokemon variations

--- data_preparation/clean_pokemon.py
+++ data_preparation/clean_pokemon.py
@@ -1,8 +1,14 @@
 def delete_pokemon_forms(pokemon):
-    # Suffixes of pokemon forms
+    # Suffixes of special pokemon forms
     forms = ('-mega', '-alola', '-galar', '-gmax')
+
     # Filtering Mega Evolutions, Alolan, Galarian and Gigantamax forms
     criteria = pokemon['name'].map(lambda name: not name.endswith(forms))
-    df = pokemon[criteria]
+    df_forms = pokemon[criteria]
+
+    variations = ('pikachu-', 'castform-', 'kyogre-', 'groudon-', 'greninja-', 'necrozma-', 'magearna-', 'eternatus-')
+    # Filtering Pikachu forms
+    pikachu_criteria = df_forms['name'].map(lambda name: not name.startswith(variations))
+    df = df_forms[pikachu_criteria]
 
     return df
