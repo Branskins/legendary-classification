dba42c4bd8c827cb5d150b28bb576147765eada5
Andres Jimenez <andresje17@gmail.com>
2023-03-24 22:59:10 -0600

    Add docstrings

--- workflows/pokemon_resources_loader.py
+++ workflows/pokemon_resources_loader.py
@@ -1,8 +1,10 @@
+"""Start point of the execution workflow"""
 from data_loading.db_populate import DatabasePersistence
 from data_loading.poke_api import get_pokemon_resources
 
 
 def main():
+    """Loads the URL for every Pokemon, this saves the Pokemon name and the URL API"""
     persistence = DatabasePersistence()
 
     df_resources = get_pokemon_resources()
